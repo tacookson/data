f2d7816b59289ca85f7dff65ad6d47a129b4d806
tacookson <alexander.cookson@gmail.com>
2020-04-24 22:45:44 -0400

    Complete script to download raw text files for US government gifts

--- us-government-gifts/src/federal-register-api-call.R
+++ us-government-gifts/src/federal-register-api-call.R
@@ -1,37 +1,41 @@
+
+# Load libraries
 library(tidyverse)
 library(httr)
 library(jsonlite)
+library(rvest)
+library(glue)
+library(lubridate)
 
-# path <- "https://www.federalregister.gov/api/v1/documents.json?"
-# 
-# fields <- list(`fields[]` = c("title", "document_number", "json_url", "publication_date"))
-# 
-# request <- GET(url = path,
-#                query = list(per_page = 20,
-#                             `fields[]` = list(`fields[]` = c("title", "document_number")),
-#                             `conditions[term]` = "gift to federal employees",
-#                             `conditions[agencies][]` = "state-department",
-#                             `conditions[type][]` = "NOTICE"))
-# 
-# status_code(request)
-# 
-# response <- content(request, as = "text", encoding = "UTF-8")
-# 
-# df <- fromJSON(response, flatten = TRUE) %>%
-#   as_tibble()
-# 
-# df_parsed <- df %>%
-#   hoist(results, title = "title", document_number = "document_number")
+# Define API path (no token required)
+path <- "https://www.federalregister.gov/api/v1/documents.json?"
 
-url <- "https://www.federalregister.gov/api/v1/documents.json?fields%5B%5D=document_number&fields%5B%5D=json_url&fields%5B%5D=publication_date&fields%5B%5D=raw_text_url&fields%5B%5D=title&per_page=500&conditions%5Bterm%5D=gifts%20to%20federal%20employees&conditions%5Bagencies%5D%5B%5D=state-department&conditions%5Btype%5D%5B%5D=NOTICE"
+# Create request for State Department Notices matching term "gift to federal employees"
+request <- GET(url = path,
+               query = list(per_page = 100,
+                            `conditions[term]` = "gift to federal employees from foreign government sources",
+                            `conditions[agencies][]` = "state-department",
+                            `conditions[type][]` = "NOTICE"))
 
-request <- GET(url)
-
-response <- content(request, as = "text", encoding = "UTF-8")
-
-df <- fromJSON(response) %>%
-  as_tibble() %>%
-  .$results %>%
+# Convert response to text
+response <- content(request, as = "text", encoding = "UTF-8") %>%
+  fromJSON(flatten = TRUE) %>%
   as_tibble()
 
-test_read <- read_lines("https://www.federalregister.gov/documents/full_text/text/2020/02/25/2020-03722.txt")
+# Construct tibble of documents and URLs for their full text
+document_list <- as_tibble(response$results, .name_repair = "universal") %>%
+  select(title, document_number, publication_date) %>%
+  filter(!str_detect(str_to_lower(title), "correction"),
+         str_detect(str_to_lower(title), "gifts to federal employees")) %>%
+  mutate(publication_date = ymd(publication_date),
+         year = year(publication_date),
+         month = str_pad(month(publication_date), 2, side = "left", pad = "0"),
+         day = str_pad(day(publication_date), 2, side = "left", pad = "0"),
+         text_url = glue("https://www.federalregister.gov/documents/full_text/text/{year}/{month}/{day}/{document_number}.txt"))
+
+# Read full text from Federal Register
+document_text <- document_list %>%
+  mutate(full_text = map(text_url, read_lines))
+
+# Pull first full text as sample
+document_text$full_text[[1]]
