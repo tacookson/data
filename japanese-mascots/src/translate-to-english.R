934eae70554e2b84153ff5dec2445b985b7e0ab3
tacookson <alexander.cookson@gmail.com>
2020-06-25 13:12:38 -0400

    Refine translation function

--- japanese-mascots/src/translate-to-english.R
+++ japanese-mascots/src/translate-to-english.R
@@ -8,11 +8,29 @@ library(googleLanguageR)
 gl_auth("./japanese-mascots/src/keys/japanese-mascots-translation-service-account.json")
 
 
+### Function definitions --------------------------------------------------------------------------
+# Wrapper around googleLanguageR::gl_translate() to return only the translated text
+get_translation <- function(origin_text) {
+  translation_object <- gl_translate(origin_text, target = "en", format = "text")
+  return(translation_object[["translatedText"]])
+}
+
 ### Test translation ------------------------------------------------------------------------------
 test_translation <- read_tsv("./japanese-mascots/src/test-for-translating.txt")
 
+# Single vector
 single_text <- test_translation %>%
   sample_n(1) %>%
   pull(organization)
 
-gl_translate(single_text, target = "en", format = "text")
+result <- gl_translate(single_text, target = "en", format = "text")
+
+# In tibble
+multi_text <- test_translation %>%
+  sample_n(3)
+
+result <- multi_text %>%
+  mutate(name_english = map(name, get_translation))
+
+result %>%
+  View()
