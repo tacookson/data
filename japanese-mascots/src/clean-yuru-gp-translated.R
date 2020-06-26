035369f30f388499ad78af6452ef78577d2177ae
tacookson <alexander.cookson@gmail.com>
2020-06-26 08:55:59 -0400

    Add description and change field name to affiliation

--- japanese-mascots/src/clean-yuru-gp-translated.R
+++ japanese-mascots/src/clean-yuru-gp-translated.R
@@ -4,15 +4,29 @@ library(tidyverse)
 
 
 ### Import and clean data -------------------------------------------------------------------------
-yuru_gp <- read_tsv("./japanese-mascots/raw/yuru-gp-translated.txt") %>%
+# Rankings
+yuru_gp_no_description <- read_tsv("./japanese-mascots/raw/yuru-gp-translated.txt") %>%
   transmute(year,
             category,
             rank,
             character_id,
             name = name_english,
             area = str_remove(area_english, "\\(") %>% str_remove("\\)"),
-            organization = organization_english,
+            affiliation = organization_english,
             image_url)
+
+# Descriptions
+descriptions <- read_tsv("./japanese-mascots/raw/descriptions-translated.txt") %>%
+  mutate(affiliation = str_extract(description_english, "Affiliation: [^\\n]*"),
+         description = str_remove(description_english, affiliation) %>% str_squish(),
+         description = ifelse(description == "", NA_character_, description)) %>%
+  select(character_id, description)
+
+# Combine rankings and descriptions
+# Note: there is duplication in the description field, but made that trade-off to get everything
+# into one dataframe
+yuru_gp <- yuru_gp_no_description %>%
+  left_join(descriptions, by = "character_id")
 
 ### Write to TXT file -----------------------------------------------------------------------------
 write_tsv(yuru_gp, "./japanese-mascots/yuru-gp.txt")
