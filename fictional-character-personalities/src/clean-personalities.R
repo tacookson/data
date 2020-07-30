c3d2c3d22dc38bd400c33f6d6409f8247d5e3dd4
tacookson <alexander.cookson@gmail.com>
2020-07-29 22:48:41 -0400

    Work out cleaning kinks with read_ods()

--- fictional-character-personalities/src/clean-personalities.R
+++ fictional-character-personalities/src/clean-personalities.R
@@ -1,17 +1,21 @@
 
 ### Load packages ---------------------------------------------------------------------------------
 library(tidyverse)
-library(readODS) # For reading the .ods data file
+library(readODS)
 library(janitor)
+library(rvest)
 
 
 ### Download and unzip raw data -------------------------------------------------------------------
+# Define path and file destination
 dest_path <- "./fictional-character-personalities/raw"
 dest_file <- paste0(dest_path, "/characters-aggregated.zip")
 
+# Download from http://openpsychometrics.org/
 download.file("http://openpsychometrics.org/_rawdata/characters-aggregated.zip",
               dest_file)
 
+# Unzip downloaded file
 unzip(dest_file, exdir = dest_path)
 
 
@@ -19,22 +23,51 @@ unzip(dest_file, exdir = dest_path)
 # Set path of raw data file
 data_path <- paste0(dest_path, "/characters-aggregated/characters-aggregated.ods")
 
-# read_ods() works, but takes several minutes (on my machine, at least)
-mean_data <- tibble(measure = "mean",
-                    data = read_ods(data_path, sheet = 1))
+# Define function to read in .ods raw data and get it into a usable format
+read_personality_data <- function(sheet_num, measure) {
+  read_ods(data_path, sheet = sheet_num) %>%
+    # First column is empty, so we need to tell as_tibble() to give it a generic name
+    as_tibble(.name_repair = "universal") %>%
+    rename(character_code = ...1) %>%
+    mutate(measure = measure) %>%
+    pivot_longer(starts_with("BAP"), names_to = "spectrum", values_to = "value")
+}
 
-ratings_data <- tibble(measure = "ratings",
-                       data = read_ods(data_path, sheet = 2))
+# Read the mean, number of ratings, and standard deviation from raw data
+personalities_raw <- tibble(sheet_num = 1:3,
+       measure = c("mean", "ratings", "sd")) %>%
+  mutate(data = map2(sheet_num, measure, read_personality_data)) %>%
+  select(data) %>%
+  unnest(data) %>%
+  pivot_wider(names_from = measure, values_from = value)
 
-sd_data <- tibble(measure = "sd",
-                  data = read_ods(data_path, sheet = 3))
+
+### Extract correspondence tables from codebook ---------------------------------------------------
+# Extract tables from codebook.html
+codebook_tables <- read_html(paste0(dest_path, "/characters-aggregated/codebook.html")) %>%
+  html_nodes("table") %>%
+  html_table()
+
+# First table gives character details
+character_codes <- as_tibble(codebook_tables[[1]]) %>%
+  rename(character_code = ID,
+         fictional_work = `Fictional work`,
+         character_name = `Character display name`)
+
+# Second table gives personality spectrum details (low --> high)
+spectrum_codes <- as_tibble(codebook_tables[[2]]) %>%
+  rename(spectrum = ID,
+         spectrum_low = `low/left anchor`,
+         spectrum_high = `high/right anchor`)
 
 
-### Combine and clean data ------------------------------------------------------------------------
-personalities <- bind_rows(mean_data, ratings_data, sd_data)
+### Look up characters and spectrum details from correspondence tables ----------------------------
+personalities <- personalities_raw %>%
+  left_join(character_codes, by = "character_code") %>%
+  left_join(spectrum_codes, by = "spectrum") %>%
+  select(character_code, fictional_work, character_name,
+         spectrum, spectrum_low, spectrum_high,
+         mean, ratings, sd)
 
-names(personalities$data)[1] <- "character_code"
-
-personalities %>%
-  mutate(data = as_tibble(data)) %>%
-  unnest(data)
+### Write to TXT ----------------------------------------------------------------------------------
+write_tsv(personalities, "./fictional-character-personalities/personalities.txt")
